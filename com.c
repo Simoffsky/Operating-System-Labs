4e3e82e91cc3bc08a957080cee92518d8b5fa542
Simoff <maks-kirsanov26@mail.ru>
2025-01-11 21:29:32 +1000

    Логика работы с портами

--- com.c
+++ com.c
@@ -2,145 +2,144 @@
 #include <string.h>
 
 #ifdef _WIN32
-    #include <windows.h>
-    #define sleep(x) Sleep(x * 1000)  // Для Windows используем Sleep() с миллисекундами
-#else
-    #include <fcntl.h>
-    #include <unistd.h>
-    #include <termios.h>
-    #include <errno.h>
-#endif
+#include <windows.h>
+#define sleep(x) Sleep(x * 1000)  // Для Windows используем Sleep() с миллисекундами
 
-// Функция для настройки порта, зависит от операционной системы
-void* setup_com_port(const char* port_name) {
-    #ifdef _WIN32
-        HANDLE hSerial;
-        DCB dcbSerialParams = {0};
-        COMMTIMEOUTS timeouts = {0};
+HANDLE setup_com_port(const char* port_name);
+int read_com(HANDLE port, char* buffer, size_t bufferSize);
+int write_com(HANDLE port, const char* data);
 
-        // Открытие порта
-        hSerial = CreateFile(port_name, GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, 0, 0);
-        if (hSerial == INVALID_HANDLE_VALUE) {
-            printf("Error opening port %s\n", port_name);
-            return NULL;
-        }
+#else //UNIX
+#include <fcntl.h>
+#include <unistd.h>
+#include <termios.h>
+#include <errno.h>
+#endif // _WIN32
 
-        // Настройка параметров порта
-        dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
-        if (!GetCommState(hSerial, &dcbSerialParams)) {
-            printf("Error getting COM state for %s\n", port_name);
-            CloseHandle(hSerial);
-            return NULL;
-        }
-        dcbSerialParams.BaudRate = CBR_9600;
-        dcbSerialParams.ByteSize = 8;
-        dcbSerialParams.StopBits = ONESTOPBIT;
-        dcbSerialParams.Parity = NOPARITY;
-        if (!SetCommState(hSerial, &dcbSerialParams)) {
-            printf("Error setting COM state for %s\n", port_name);
-            CloseHandle(hSerial);
-            return NULL;
-        }
+#ifdef _WIN32
+#include <windows.h>
 
-        // Настройка тайм-аутов
-        timeouts.ReadIntervalTimeout = 50;
-        timeouts.ReadTotalTimeoutConstant = 50;
-        timeouts.ReadTotalTimeoutMultiplier = 10;
-        timeouts.WriteTotalTimeoutConstant = 50;
-        timeouts.WriteTotalTimeoutMultiplier = 10;
-        if (!SetCommTimeouts(hSerial, &timeouts)) {
-            printf("Error setting timeouts for %s\n", port_name);
-            CloseHandle(hSerial);
-            return NULL;
-        }
+HANDLE setup_com_port(const char* port_name) {
+    HANDLE hSerial;
+    DCB dcbSerialParams = {0};
+    COMMTIMEOUTS timeouts = {0};
 
-        return hSerial;  // Возвращаем дескриптор порта
+    // Открытие порта
+    hSerial = CreateFile(port_name, GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, 0, 0);
+    if (hSerial == INVALID_HANDLE_VALUE) {
+        printf("Error opening port %s\n", port_name);
+        return INVALID_HANDLE_VALUE;
+    }
 
-    #else
-        int fd;
-        struct termios options;
+    // Настройка параметров порта
+    dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
+    if (!GetCommState(hSerial, &dcbSerialParams)) {
+        printf("Error getting COM state for %s\n", port_name);
+        CloseHandle(hSerial);
+        return INVALID_HANDLE_VALUE;
+    }
+    dcbSerialParams.BaudRate = CBR_9600;
+    dcbSerialParams.ByteSize = 8;
+    dcbSerialParams.StopBits = ONESTOPBIT;
+    dcbSerialParams.Parity = NOPARITY;
+    if (!SetCommState(hSerial, &dcbSerialParams)) {
+        printf("Error setting COM state for %s\n", port_name);
+        CloseHandle(hSerial);
+        return INVALID_HANDLE_VALUE;
+    }
 
-        // Открытие порта
-        fd = open(port_name, O_RDWR | O_NOCTTY | O_NDELAY);
-        if (fd == -1) {
-            perror("Error opening port");
-            return NULL;
-        }
+    // Настройка тайм-аутов
+    timeouts.ReadIntervalTimeout = 50;
+    timeouts.ReadTotalTimeoutConstant = 50;
+    timeouts.ReadTotalTimeoutMultiplier = 10;
+    timeouts.WriteTotalTimeoutConstant = 50;
+    timeouts.WriteTotalTimeoutMultiplier = 10;
+    if (!SetCommTimeouts(hSerial, &timeouts)) {
+        printf("Error setting timeouts for %s\n", port_name);
+        CloseHandle(hSerial);
+        return INVALID_HANDLE_VALUE;
+    }
 
-        // Настройка параметров порта
-        if (tcgetattr(fd, &options) < 0) {
-            perror("Error getting port settings");
-            close(fd);
-            return NULL;
-        }
-        cfsetispeed(&options, B9600);  // Устанавливаем скорость порта
-        cfsetospeed(&options, B9600);  // Устанавливаем скорость порта
+    return hSerial;  // Возвращаем дескриптор порта
+}
 
-        options.c_cflag &= ~PARENB;    // Без четности
-        options.c_cflag &= ~CSTOPB;    // Один стоповый бит
-        options.c_cflag &= ~CSIZE;
-        options.c_cflag |= CS8;        // 8 бит данных
-        options.c_cflag &= ~CRTSCTS;   // Без контроля потока
-        options.c_cflag |= CREAD | CLOCAL;  // Разрешаем чтение и игнорируем модемный контроль
+int read_com(HANDLE hSerial, char* buffer, DWORD bufferSize) {
+    DWORD bytesRead;
+    if (!ReadFile(hSerial, buffer, bufferSize - 1, &bytesRead, NULL)) {
+        printf("Error reading from COM port\n");
+        return -1;
+    }
+    buffer[bytesRead] = '\0';  // Завершающий ноль для строки
+    printf("Bytes Read: %d\n", bytesRead);
+    printf("Data received: %s\n", buffer);
+    return bytesRead;
+}
 
-        // Настройка тайм-аутов
-        options.c_cc[VMIN] = 1;       // Ожидание хотя бы одного байта
-        options.c_cc[VTIME] = 5;      // Тайм-аут в 0.5 секунд
+int write_com(HANDLE hSerial, const char* data) {
+    DWORD bytesWritten;
+    if (!WriteFile(hSerial, data, strlen(data), &bytesWritten, NULL)) {
+        printf("Error writing to COM port\n");
+        return -1;
+    }
+    printf("Data sent: %s\n", data);
+    return bytesWritten;
+}
+#else //Unix
 
-        if (tcsetattr(fd, TCSANOW, &options) < 0) {
-            perror("Error setting port settings");
-            close(fd);
-            return NULL;
-        }
+int setup_com_port(const char* port_name) {
+    int fd;
+    struct termios options;
 
-        return (void*)fd;  // Возвращаем файловый дескриптор порта
-    #endif
+    fd = open(port_name, O_RDWR | O_NOCTTY | O_NDELAY);
+    if (fd == -1) {
+        perror("Error opening port");
+        return -1;
+    }
+
+    if (tcgetattr(fd, &options) < 0) {
+        perror("Error getting port settings");
+        close(fd);
+        return -1;
+    }
+    cfsetispeed(&options, B9600);  
+
+    options.c_cflag &= ~PARENB;    
+    options.c_cflag &= ~CSTOPB;   
+    options.c_cflag &= ~CSIZE;
+    options.c_cflag |= CS8;       
+    options.c_cflag |= CREAD | CLOCAL;  
+    options.c_cc[VMIN] = 1;      
+    options.c_cc[VTIME] = 5;     
+
+    if (tcsetattr(fd, TCSANOW, &options) < 0) {
+        perror("Error setting port settings");
+        close(fd);
+        return -1;
+    }
+
+    return fd;  
 }
 
 // Чтение из порта
-int read_com(void* port, char* buffer, size_t bufferSize) {
-    #ifdef _WIN32
-        HANDLE hSerial = (HANDLE)port;
-        DWORD bytesRead;
-        if (!ReadFile(hSerial, buffer, bufferSize - 1, &bytesRead, NULL)) {
-            printf("Error reading from COM port\n");
-            return -1;
-        }
-        buffer[bytesRead] = '\0';  // Завершающий ноль для строки
-        printf("Bytes Read: %d\n", bytesRead);
-        return bytesRead;
-    #else
-        int fd = (int)(intptr_t)port;
-        int bytesRead = read(fd, buffer, bufferSize - 1);
-        if (bytesRead < 0) {
-            perror("Error reading from serial port");
-            return -1;
-        }
-        buffer[bytesRead] = '\0';  // Завершающий ноль для строки
-        return bytesRead;
-    #endif
+int read_com(int fd, char* buffer, size_t bufferSize) {
+    int bytesRead = read(fd, buffer, bufferSize - 1);
+    if (bytesRead < 0) {
+        perror("Error reading from serial port");
+        return -1;
+    }
+    buffer[bytesRead] = '\0'; 
+    return bytesRead;
 }
 
 // Запись в порт
-int write_com(void* port, const char* data) {
-    #ifdef _WIN32
-        HANDLE hSerial = (HANDLE)port;
-        DWORD bytesWritten;
-        if (!WriteFile(hSerial, data, strlen(data), &bytesWritten, NULL)) {
-            printf("Error writing to COM port\n");
-            return -1;
-        }
-        printf("Data sent: %s\n", data);
-        return bytesWritten;
-    #else
-        int fd = (int)(intptr_t)port;
-        int bytesWritten = write(fd, data, strlen(data));
-        if (bytesWritten < 0) {
-            perror("Error writing to serial port");
-            return -1;
-        }
-        printf("Data sent: %s\n", data);
-        return bytesWritten;
-    #endif
+int write_com(int fd, const char* data) {
+    int bytesWritten = write(fd, data, strlen(data));
+    if (bytesWritten < 0) {
+        perror("Error writing to serial port");
+        return -1;
+    }
+    printf("Data sent: %s\n", data);
+    return bytesWritten;
 }
 
+#endif //_WIN32
--- main.c
+++ main.c
@@ -1,37 +1,37 @@
 #include "com.c"
 
 int main() {
-    void* portRead;
-    void* portWrite;
 #ifdef _WIN32
+    HANDLE portRead;
+    HANDLE portWrite;
     const char* portReadName = "COM1"; 
     const char* portWriteName = "COM2"; 
 #else
-    const char* portReadName = "/dev/ttyS0";  
-    const char* portWriteName = "/dev/ttyS1"; 
+    int portRead;
+    int portWrite;
+    const char* portReadName = "/dev/pts/7";  
+    const char* portWriteName = "/dev/pts/8"; 
 #endif 
     // Настройка COM портов
     portRead = setup_com_port(portReadName);
-    if (portRead == NULL) {
+    if (portRead == -1) {
         return 1;
     }
 
     portWrite = setup_com_port(portWriteName);
-    if (portWrite == NULL) {
+    if (portWrite == -1) {
         return 1;
     }
 
-    // Бесконечный цикл чтения и записи
     char buffer[128];
     const char* dataToSend = "Hello, COM!";
     while (1) {
-        // Чтение данных
-        read_com(portRead, buffer, sizeof(buffer));
 
-        // Запись данных
         write_com(portWrite, dataToSend);
 
-        sleep(1);  // Пауза между операциями
+        read_com(portRead, buffer, sizeof(buffer));
+        printf("readed: %s\n", buffer);
+        sleep(1); 
     }
 
     // Закрытие портов
@@ -39,8 +39,8 @@ int main() {
         CloseHandle(portRead);
         CloseHandle(portWrite);
     #else
-        close((int)(intptr_t)portRead);
-        close((int)(intptr_t)portWrite);
+        close(portRead);
+        close(portWrite);
     #endif
 
     return 0;
